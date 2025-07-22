75e7712a128f77b6bee30132934c4a22db725b56
iconsumeplutonium <uzymc242@outlook.com>
2025-07-21 20:48:16 -0700

    tweak deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: "https://iconsumeplutonium.github.io",
-	base: "personal-website"
+	base: "/personal-website"
 });
